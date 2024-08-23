038d6de5d4b27a60560f349d762c22d3260258d0
dhanianwr <dhanyaries@gmail.com>
2024-08-24 04:16:52 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+const navbarScroll = document.querySelector(".navbar");
+document.addEventListener("scroll", function() {
+  if (window.scrollY > 0 && !navbarScroll.contains(document.activeElement)) {
+    navbarScroll.style.backgroundColor = "rgba(1, 1, 3, 0.8)";
+    navbarScroll.style.transition = "background-color 0.8s ease-in-out"
+  } else {
+    navbarScroll.style.backgroundColor = ""; // reset to original color
+  }
+});
